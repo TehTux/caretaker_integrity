1cb5eaf3ef5caa606316273f665036e1c9ba0536
Jan Haffner <jan.haffner@dkd.de>
2017-08-29 15:17:17 +0200

    [TASK] Move service registrations to localconf

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,11 +1,28 @@
 <?php
 
+use \TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 use \TYPO3\CMS\Core\Utility\VersionNumberUtility;
 
 if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
 
 if (VersionNumberUtility::convertVersionNumberToInteger(TYPO3_version) >= 4001000) {
 	$TYPO3_CONF_VARS['SC_OPTIONS']['GLOBAL']['cliKeys']['caretaker_integrity'] = array('EXT:caretaker_integrity/classes/class.tx_caretakerintegrity_Cli.php','_CLI_caretaker');
+}
+
+// Register caretaker services
+if (ExtensionManagementUtility::isLoaded('caretaker')) {
+	tx_caretaker_ServiceHelper::registerCaretakerTestService(
+		$_EXTKEY,
+		'services',
+		'tx_caretakerintegrity_CheckCoreIntegrity',
+		'Integrity -> Check TYPO3-core source integrity',
+		'Find changed files in remote TYPO3-Core'
+	);
+	tx_caretaker_ServiceHelper::registerCaretakerTestService(
+		$_EXTKEY, 'services',
+		'tx_caretakerintegrity_CheckFolderIntegrity',
+		'Integrity -> Check integrity of a folder or file',
+		'Find changed files in remote folder');
 }
 
 ?>
